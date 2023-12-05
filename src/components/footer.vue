255a6034235a327d35e3fd7001d8bace48a89b52
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2023-12-05 00:01:43 -0300

    feature footer

---
+++ src/components/footer.vue
@@ -0,0 +1,26 @@
+<template>
+<footer class="flex justify-between p-5">
+	<div class="flex justify-start items-center gap-3 ">
+		<span>2023, made in brazil</span>
+		<a>Sintax</a>
+	</div>
+	<nav class="flex justify-end items-center gap-5 " >
+		<a>Licensa</a>
+		<a>Documentação</a>
+		<a>Suporte</a>
+		<a>contato</a>
+	</nav>
+</footer>
+
+</template>
+
+<script>
+export default {
+	name: "Footer",
+};
+</script>
+<style lang="postcss" scoped>
+a{
+	@apply text-sm
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,23 +3,31 @@ import Cabecalho from "../components/cabecalho.vue";
 import Count from "../components/count.vue";
 import Table from "../components/table.vue";
 import Navegation from "../components/navegation.vue";
+import Footer from "../components/footer.vue";
 </script>
 
 <template>
-  <div class="flex h-screen">
-    <aside class="w-1/5 h-full ">
-      <navegation />
-    </aside>
-    <main class="w-4/5 h-screen flex flex-col">
-      <section class="h-20 ">
-        <cabecalho />
-      </section>
-      <section class="h-40 ">
-        <count />
-      </section>
-      <section class="flex-1">
-        <Table/>
-      </section>
-    </main>
+  <div class="flex flex-col min-h-screen"> <!-- Altere para flex-col e min-h-screen -->
+    <div class="flex flex-1"> <!-- Adicione um div envolvendo aside e main -->
+      <aside class="w-1/5">
+        <navegation />
+      </aside>
+      <main class="w-4/5 flex flex-col">
+        <section class="h-20">
+          <cabecalho />
+        </section>
+        <section class="h-40">
+          <count />
+        </section>
+        <section class="flex-1">
+          <Table/>
+        </section>
+				<Footer/>
+      </main>
+    </div>
+    <footer class="w-full h-40">
+     
+    </footer>
   </div>
 </template>
+
