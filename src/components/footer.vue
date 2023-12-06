55658da272a9906a56d2f747cd7e00a25e20d82f
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2023-12-05 23:57:09 -0300

    alterações footer

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,17 +1,16 @@
 <template>
-<footer class="flex justify-between p-5">
-	<div class="flex justify-start items-center gap-3 ">
-		<span>2023, made in brazil</span>
-		<a>Sintax</a>
-	</div>
-	<nav class="flex justify-end items-center gap-5 " >
-		<a>Licensa</a>
-		<a>Documentação</a>
-		<a>Suporte</a>
-		<a>contato</a>
-	</nav>
-</footer>
-
+	<footer id="footer">
+			<div>
+					<span>2023, made in brazil</span>
+					<a>Sintax</a>
+			</div>
+			<nav>
+					<a>Licensa</a>
+					<a>Documentação</a>
+					<a>Suporte</a>
+					<a>contato</a>
+			</nav>
+	</footer>
 </template>
 
 <script>
@@ -19,8 +18,18 @@ export default {
 	name: "Footer",
 };
 </script>
+
 <style lang="postcss" scoped>
-a{
-	@apply text-sm
+#footer {
+	@apply flex justify-between p-5;
+}
+#footer > div:first-of-type {
+	@apply flex justify-start items-center gap-3;
+}
+#footer > nav {
+	@apply flex justify-end items-center gap-5;
+}
+#footer a {
+	@apply text-sm;
 }
 </style>
