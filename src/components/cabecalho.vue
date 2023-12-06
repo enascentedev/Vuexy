7913316ed127a7d9d4c8c68e897f571633140cce
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2023-12-06 19:35:19 -0300

    migrando classes header

--- src/components/cabecalho.vue
+++ src/components/cabecalho.vue
@@ -1,7 +1,7 @@
 <template>
-  <header class="flex justify-between items-center p-5 text-lg shadow-2xl">
+  <header id="header">
     <i class="fas fa-search"></i>
-    <div class="flex items-center gap-5">
+    <div>
       <i class="fas fa-user"></i>
       <i class="fas fa-bell"></i>
       <i class="fas fa-user-circle"></i>
@@ -9,9 +9,26 @@
   </header>
 </template>
 
-
 <script>
 export default {
-name: "Cabecalho",
+  name: "Cabecalho",
 };
 </script>
+
+<style lang="postcss" scoped>
+#header {
+  @apply flex justify-between items-center p-5 text-lg shadow-2xl
+}
+
+#header > i.fas.fa-search {
+ 
+}
+
+#header > div {
+  @apply flex items-center gap-5;
+}
+
+#header > div > .fas {
+  
+}
+</style>
